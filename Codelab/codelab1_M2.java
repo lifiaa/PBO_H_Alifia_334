650392479a559426dbacb7436af9dab12f64a684
fiaa <129140730+lifiaa@users.noreply.github.com>
2025-03-09 10:22:18 +0700

    first

---
+++ Codelab/codelab1_M2.java
@@ -0,0 +1,30 @@
+java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Hewan hewan1 = new Hewan("Kucing", "Mamalia", "Nyann~~");
+        Hewan hewan2 = new Hewan("Anjing", "Mamalia", "Woof-Woof!!");
+
+        hewan1.tampilkanInfo();
+        hewan2.tampilkanInfo();
+    }
+}
+
+public class Hewan {
+    String nama;
+    String jenis;
+    String suara;
+
+    Hewan(String nama, String jenis, String suara) {
+        this.nama = nama;
+        this.jenis = jenis;
+        this.suara = suara;
+    }
+
+    void tampilkanInfo() {
+        System.out.println("Nama: " + nama);
+        System.out.println("Jenis: " + jenis);
+        System.out.println("Suara: " + suara);
+        System.out.println();
+    }
+}
